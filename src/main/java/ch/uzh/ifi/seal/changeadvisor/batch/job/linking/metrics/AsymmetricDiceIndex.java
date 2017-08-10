241c96caf8de20e94ff4164bda8793ed2981bc1d
Alex Hofmann <alexhofmann@gmail.com>
2017-08-10 12:55:58 +0200

    Implement asymmetric dice index.

--- src/main/java/ch/uzh/ifi/seal/changeadvisor/batch/job/linking/metrics/AsymmetricDiceIndex.java
+++ src/main/java/ch/uzh/ifi/seal/changeadvisor/batch/job/linking/metrics/AsymmetricDiceIndex.java
@@ -1,22 +1,44 @@
 package ch.uzh.ifi.seal.changeadvisor.batch.job.linking.metrics;
 
-import java.util.Set;
+import com.google.common.base.Splitter;
 
-public class AsymmetricDiceIndex implements SimilarityMetric {
+import java.util.Collection;
+import java.util.List;
+
+public final class AsymmetricDiceIndex implements SimilarityMetric {
+
+    private final MetricUtils metricUtils = new MetricUtils();
 
     @Override
     public double similarity(String document1, String document2) {
-
-        return 0;
+        List<String> docTokens1 = Splitter.on(" ").splitToList(document1);
+        List<String> docTokens2 = Splitter.on(" ").splitToList(document2);
+        return similarity(docTokens1, docTokens2);
     }
 
-    int countOverlappingWords(Set<String> doc1, Set<String> doc2) {
-        int counter = 0;
-        for (String s1 : doc1) {
-            if (doc2.contains(s1)) {
-                counter++;
-            }
+    @Override
+    public double similarity(Collection<String> document1, Collection<String> document2) {
+        if (document1.isEmpty() || document2.isEmpty()) {
+            return 0.0;
         }
-        return counter;
+
+        Double overlap = (double) metricUtils.countOverlappingWords(document1, document2);
+        Double result = 2 * overlap;
+
+        if (document1.size() < document2.size()) {
+            result = result / document1.size();
+        } else {
+            result = result / document2.size();
+        }
+
+        if (result > 1.0) {
+            result = 1.0;
+        }
+
+        if (result.toString().contains("E")) {
+            result = 0.0;
+        }
+
+        return result;
     }
 }
--- src/test/java/ch/uzh/ifi/seal/changeadvisor/batch/job/linking/metrics/AsymmetricDiceIndexTest.java
+++ src/test/java/ch/uzh/ifi/seal/changeadvisor/batch/job/linking/metrics/AsymmetricDiceIndexTest.java
@@ -5,22 +5,55 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.core.Is.is;
 
 public class AsymmetricDiceIndexTest {
 
+    private final AsymmetricDiceIndex asymmetricDiceIndex = new AsymmetricDiceIndex();
+
     @Test
-    public void countOverlappingWords() throws Exception {
+    public void similarity() throws Exception {
         Set<String> doc1 = Sets.newHashSet("foo", "bar", "baz", "hello", "world");
         Set<String> doc2 = Sets.newHashSet("foo", "bar", "zzz", "www", "world");
         Set<String> doc3 = Sets.newHashSet("aa", "bb", "zzz", "www", "cc");
+        Set<String> doc4 = Sets.newHashSet("aa", "bb", "zzz", "hello", "cc");
+        Set<String> doc5 = IntStream.range(0, 100).mapToObj(i -> UUID.randomUUID().toString()).collect(Collectors.toSet());
+        doc5.add("foo");
 
-        int i = new AsymmetricDiceIndex().countOverlappingWords(doc1, doc2);
-        Assert.assertThat(i, is(3));
 
-        i = new AsymmetricDiceIndex().countOverlappingWords(doc1, doc3);
-        Assert.assertThat(i, is(0));
+        double similarity = asymmetricDiceIndex.similarity(doc1, doc2);
+
+        Assert.assertThat(similarity, is(1.0));
+
+        similarity = asymmetricDiceIndex.similarity(doc2, doc3);
+        Assert.assertThat(similarity, is(0.8));
+
+        similarity = asymmetricDiceIndex.similarity(doc2, doc4);
+        Assert.assertThat(similarity, is(0.4));
+
+        similarity = asymmetricDiceIndex.similarity(doc1, doc5);
+        Assert.assertThat(similarity, is(0.4));
     }
 
+    @Test
+    public void noSimilarity() throws Exception {
+        Set<String> doc1 = Sets.newHashSet("foo", "bar", "baz", "hello", "world");
+        Set<String> doc2 = Sets.newHashSet("aa", "bb", "zzz", "www", "cc");
+        Set<String> doc3 = IntStream.range(0, 99).mapToObj(i -> UUID.randomUUID().toString()).collect(Collectors.toSet());
+        Set<String> doc4 = IntStream.range(0, 99).mapToObj(i -> UUID.randomUUID().toString()).collect(Collectors.toSet());
+
+
+        double similarity = asymmetricDiceIndex.similarity(doc1, doc2);
+        Assert.assertThat(similarity, is(0.0));
+
+        similarity = asymmetricDiceIndex.similarity(doc1, doc4);
+        Assert.assertThat(similarity, is(0.0));
+
+        similarity = asymmetricDiceIndex.similarity(doc3, doc4);
+        Assert.assertThat(similarity, is(0.0));
+    }
 }
