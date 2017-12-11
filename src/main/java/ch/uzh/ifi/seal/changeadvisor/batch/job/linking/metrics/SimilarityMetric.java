6f8f898d0074419df768b4983aebc9f9c42d6ca7
Alexander Hofmann <alexhofmann@gmail.com>
2017-12-11 01:00:00 +0100

    Slight change doc on similarity metric.

--- src/main/java/ch/uzh/ifi/seal/changeadvisor/batch/job/linking/metrics/SimilarityMetric.java
+++ src/main/java/ch/uzh/ifi/seal/changeadvisor/batch/job/linking/metrics/SimilarityMetric.java
@@ -5,19 +5,19 @@ import java.util.Collection;
 public interface SimilarityMetric {
 
     /**
-     * Compute similarity between two corpuses.
+     * Compute similarity between two documents.
      *
-     * @param document1 review text.
-     * @param document2 code element text.
+     * @param document1 first document.
+     * @param document2 second document.
      * @return
      */
     double similarity(String document1, String document2);
 
     /**
-     * Compute similarity between two corpuses.
+     * Compute similarity between two documents.
      *
-     * @param document1 review text tokens.
-     * @param document2 code element text tokens.
+     * @param document1 first document as tokens.
+     * @param document2 second document tokens.
      * @return
      */
     double similarity(Collection<String> document1, Collection<String> document2);
